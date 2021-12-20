330bb82e0ec92d640ab75f3f095c2783ba78f24a
Emmanuel-Lopes <esaldanha@bionexo.com>
2021-12-20 10:59:26 -0300

    pequenas mudanças no formulario

--- resources/views/formulario/formulario.blade.php
+++ resources/views/formulario/formulario.blade.php
@@ -14,34 +14,34 @@
         <p class="font-monospace fs-4 fw-bold text-center">Informações Pessoais<hr class="mb-4"></p>
         <div class="mb-4">
             <i class="far fa-user"></i>
-            <label for="nome" class="form-label">Nome completo <abbr title="Este campo é obrigatório."> *</abbr></label>
+            <label for="nome" class="form-label fw-bold">Nome completo <abbr title="Este campo é obrigatório."> *</abbr></label>
             <input type="text" name="nome" id="nome" class="form-control" placeholder="Digite o nome completo aqui.">
         </div>
         <div class="mb-4 row g-5 align-items-center justify-content-between">
             <div class="col-4">
                 <i class="fas fa-id-card"></i>
-                <label for="cpf" class="form-label">CPF <abbr title="Este campo é obrigatório."> *</abbr></label>
+                <label for="cpf" class="form-label fw-bold">CPF <abbr title="Este campo é obrigatório."> *</abbr></label>
                 <input type="number" name="cpf" id="cpf" class="form-control" placeholder="Digite somente números.">
             </div>
             <div class="col-4">
                 <i class="fas fa-fingerprint"></i>
-                <label for="rg" class="form-label">RG</label>
+                <label for="rg" class="form-label fw-bold">RG</label>
                 <input type="number" name="rg" id="rg" class="form-control" placeholder="Digite somente números.">
             </div>
             <div class="col-4">
                 <i class="far fa-calendar-alt"></i>
-                <label class="form-label" for="nasc">Data de nascimento <abbr title="Este campo é obrigatório."> *</abbr></label>
+                <label class="form-label fw-bold" for="nasc">Data de nascimento <abbr title="Este campo é obrigatório."> *</abbr></label>
                 <input type="date" name="nasc" id="nasc" class="form-control">
             </div>
         </div>
         <div class="mb-4">
             <i class="fas fa-female"></i>
-            <label for="mae" class="form-label">Nome da mãe</label>
+            <label for="mae" class="form-label fw-bold">Nome da mãe</label>
             <input type="text" name="mae" id="mae" class="form-control" placeholder="Digite o nome completo aqui.">
         </div>
         <div class="">
             <i class="fas fa-male"></i>
-            <label for="pai" class="form-label">Nome do pai</label>
+            <label for="pai" class="form-label fw-bold">Nome do pai</label>
             <input type="text" name="pai" id="pai" class="form-control" placeholder="Digite o nome completo aqui.">
         </div>
     </div>
@@ -50,17 +50,17 @@
         <div class="row align-items-center mb-4">
             <div class="col-4">
                 <i class="fas fa-at"></i>
-                <label for="email" class="form-label">Email <abbr title="Este campo é obrigatório."> *</abbr></label>
+                <label for="email" class="form-label fw-bold">Email <abbr title="Este campo é obrigatório."> *</abbr></label>
                 <input type="email" name="email" id="email" class="form-control" placeholder="aloha@mail.ok">
             </div>
             <div class="col-4">
                 <i class="fas fa-mobile-alt"></i>
-                <label for="tel1" class="form-label">Telefone 1 <abbr title="Este campo é obrigatório."> *</abbr></label>
+                <label for="tel1" class="form-label fw-bold">Telefone 1 <abbr title="Este campo é obrigatório."> *</abbr></label>
                 <input type="number" name="tel1" id="tel1" class="form-control" placeholder="Digite somente números.">
             </div>
             <div class="col-4">
                 <i class="fas fa-phone"></i>
-                <label for="tel2" class="form-label">Telefone 2</label>
+                <label for="tel2" class="form-label fw-bold">Telefone 2</label>
                 <input type="number" name="tel2" id="tel2" class="form-control" placeholder="Digite somente números.">
             </div>
         </div>
@@ -69,33 +69,33 @@
         <p class="font-monospace fs-4 fw-bold text-center">Endereço<hr class="mb-4"></p>
         <div class="mb-4">
             <i class="fas fa-home"></i>
-            <label for="logr" class="form-label">Logradouro <abbr title="Este campo é obrigatório.">* </abbr></label>
+            <label for="logr" class="form-label fw-bold">Logradouro <abbr title="Este campo é obrigatório.">* </abbr></label>
             <input type="text" name="logr" id="logr" class="form-control" placeholder="Digite a rua, avenida, travessa...">
         </div>
         <div class="row mb-4 align-items-center">
             <div class="col-3">
                 <i class="fas fa-sort-numeric-up"></i>
-                <label for="num" class="form-label">Número <abbr title="Este campo é obrigatório."> *</abbr></label>
+                <label for="num" class="form-label fw-bold">Número <abbr title="Este campo é obrigatório."> *</abbr></label>
                 <input type="text" name="num" id="num" class="form-control" placeholder="Digite o número do endereço.">
             </div>
             <div class="col-9">
                 <i class="fas fa-map-marked-alt"></i>
-                <label for="comp" class="form-label">Complemento</label>
+                <label for="comp" class="form-label fw-bold">Complemento</label>
                 <input type="text" name="comp" id="comp" class="form-control" placeholder="Digite informações complementares.">
             </div>
         </div>
         <div class="row align-items-center">
             <div class="col-3">
-                <label for="cep">CEP <abbr title="Este campo é obrigatório."> *</abbr></label>
+                <label for="cep" class="form-label fw-bold">CEP <abbr title="Este campo é obrigatório."> *</abbr></label>
                 <input type="number" name="cep" id="cep" class="form-control" placeholder="Digite somente números.">
             </div>
             <div class="col-4">
-                <label for="uf">UF <abbr title="Este campo é obrigatório."> *</abbr></label>
+                <label for="uf" class="form-label fw-bold">UF <abbr title="Este campo é obrigatório."> *</abbr></label>
                 <input type="text" name="uf" id="uf" class="form-control" placeholder="Digite somente as iniciais.">
             </div>
             <div class="col-5">
                 <i class="fas fa-flag"></i>
-                <label for="pais">País <abbr title="Este campo é obrigatório."> *</abbr></label>
+                <label for="pais" class="form-label fw-bold">País <abbr title="Este campo é obrigatório."> *</abbr></label>
                 <input type="text" name="pais" id="pais" class="form-control" placeholder="Digite o país.">
             </div>
         </div>
