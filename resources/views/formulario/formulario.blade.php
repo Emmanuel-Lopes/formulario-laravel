8103a2c409ffa25898e291e9e5c25c93f389bd2e
Emmanuel-Lopes <esaldanha@bionexo.com>
2021-12-30 10:05:02 -0300

    atualizei os metodos de validacao

--- resources/views/formulario/formulario.blade.php
+++ resources/views/formulario/formulario.blade.php
@@ -30,34 +30,39 @@
             <div class="mb-4">
                 <i class="far fa-user"></i>
                 <label for="nome" class="form-label fw-bold">Nome completo <abbr title="Este campo é obrigatório."> *</abbr></label>
-                <input type="text"  name="nome" id="nome" class="form-control" placeholder="Digite o nome completo aqui." onchange="checkForErrors(this.value)">
+                <input type="text"  name="nome" id="nome" class="form-control" placeholder="Digite o nome completo aqui." 
+                onchange="validarNomeCompleto(this.value, document.querySelector('#nome'))">
             </div>
             <div class="mb-4 row g-5 align-items-center justify-content-between">
                 <div class="col-4">
                     <i class="fas fa-id-card"></i>
                     <label for="cpf" class="form-label fw-bold">CPF <abbr title="Este campo é obrigatório."> *</abbr></label>
-                    <input type="number" name="cpf" id="cpf" class="form-control" placeholder="Digite somente números." onchange="checkForErrors(this.value)">
+                    <input type="number" name="cpf" id="cpf" class="form-control" placeholder="Digite somente números." 
+                    onchange="validarCPF(this.value, document.querySelector('#cpf'))">
                 </div>
                 <div class="col-4">
                     <i class="fas fa-fingerprint"></i>
                     <label for="rg" class="form-label fw-bold">RG</label>
-                    <input type="number" name="rg" id="rg" class="form-control" placeholder="Digite somente números." onchange="checkForErrors(this.value)">
+                    <input type="number" name="rg" id="rg" class="form-control" placeholder="Digite somente números." 
+                    onchange="validarRG(this.value, document.querySelector('#rg'))">
                 </div>
                 <div class="col-4">
                     <i class="far fa-calendar-alt"></i>
                     <label class="form-label fw-bold" for="nasc">Data de nascimento <abbr title="Este campo é obrigatório."> *</abbr></label>
-                    <input type="date" name="nasc" id="nasc" class="form-control" onchange="checkForErrors(this.value)">
+                    <input type="date" name="nasc" id="nasc" class="form-control" onchange="getInputValueAndName(this.value, this.getAttribute('name'), document.querySelector('#nasc'))">
                 </div>
             </div>
             <div class="mb-4">
                 <i class="fas fa-female"></i>
                 <label for="mae" class="form-label fw-bold">Nome da mãe</label>
-                <input type="text" name="mae" id="mae" class="form-control" placeholder="Digite o nome completo aqui." onchange="checkForErrors(this.value)">
+                <input type="text" name="mae" id="mae" class="form-control" placeholder="Digite o nome completo aqui." 
+                onchange="validarNomeCompleto(this.value, document.querySelector('#mae'))">
             </div>
             <div class="">
                 <i class="fas fa-male"></i>
                 <label for="pai" class="form-label fw-bold">Nome do pai</label>
-                <input type="text" name="pai" id="pai" class="form-control" placeholder="Digite o nome completo aqui." onchange="checkForErrors(this.value)">
+                <input type="text" name="pai" id="pai" class="form-control" placeholder="Digite o nome completo aqui." 
+                onchange="validarNomeCompleto(this.value, document.querySelector('#pai'))">
             </div>
         </div>
         <div class="card p-4 mb-3">
@@ -66,17 +71,20 @@
                 <div class="col-4">
                     <i class="fas fa-at"></i>
                     <label for="email" class="form-label fw-bold">Email <abbr title="Este campo é obrigatório."> *</abbr></label>
-                    <input type="email" name="email" id="email" class="form-control" placeholder="aloha@mail.ok" onchange="checkForErrors(this.value)">
+                    <input type="email" name="email" id="email" class="form-control" placeholder="aloha@mail.ok"
+                     onchange="validarEmail(this.value, document.querySelector('#email'))">
                 </div>
                 <div class="col-4">
                     <i class="fas fa-mobile-alt"></i>
                     <label for="tel1" class="form-label fw-bold">Telefone 1 <abbr title="Este campo é obrigatório."> *</abbr></label>
-                    <input type="number" name="tel1" id="tel1" class="form-control" placeholder="Digite somente números." onchange="checkForErrors(this.value)">
+                    <input type="number" name="tel1" id="tel1" class="form-control" placeholder="Digite somente números." 
+                    onchange="validarTelefone(this.value, document.querySelector('#tel1'))">
                 </div>
                 <div class="col-4">
                     <i class="fas fa-phone"></i>
                     <label for="tel2" class="form-label fw-bold">Telefone 2</label>
-                    <input type="number" name="tel2" id="tel2" class="form-control" placeholder="Digite somente números." onchange="checkForErrors(this.value)">
+                    <input type="number" name="tel2" id="tel2" class="form-control" placeholder="Digite somente números." 
+                    onchange="validarTelefone(this.value, document.querySelector('#tel2'))">
                 </div>
             </div>
         </div>
@@ -85,38 +93,45 @@
             <div class="mb-4">
                 <i class="fas fa-home"></i>
                 <label for="logr" class="form-label fw-bold">Logradouro <abbr title="Este campo é obrigatório.">* </abbr></label>
-                <input type="text" name="logr" id="logr" class="form-control" placeholder="Digite a rua, avenida, travessa..." onchange="checkForErrors(this.value)">
+                <input type="text" name="logr" id="logr" class="form-control" placeholder="Digite a rua, avenida, travessa..." 
+                onchange="validarLOGR(this.value, document.querySelector('#logr'))">
             </div>
             <div class="row mb-4 align-items-center">
                 <div class="col-2">
                     <i class="fas fa-sort-numeric-up"></i>
                     <label for="num" class="form-label fw-bold">Número <abbr title="Este campo é obrigatório."> *</abbr></label>
-                    <input type="text" name="num" id="num" class="form-control" placeholder="Digite o número do endereço." onchange="checkForErrors(this.value)">
+                    <input type="text" name="num" id="num" class="form-control" placeholder="Digite o número do endereço." 
+                    onchange="validarNumero(this.value, document.querySelector('#num'))">
                 </div>
                 <div class="col-10">
                     <i class="fas fa-map-marked-alt"></i>
                     <label for="comp" class="form-label fw-bold">Complemento</label>
-                    <input type="text" name="comp" id="comp" class="form-control" placeholder="Digite informações complementares." onchange="checkForErrors(this.value)">
+                    <input type="text" name="comp" id="comp" class="form-control" placeholder="Digite informações complementares." 
+                    onchange="validarComp(this.value, document.querySelector('#comp'))">
                 </div>
             </div>
             <div class="row align-items-center">
                 <div class="col-3">
                     <label for="cep" class="form-label fw-bold">CEP <abbr title="Este campo é obrigatório."> *</abbr></label>
-                    <input type="number" name="cep" id="cep" class="form-control" placeholder="Digite somente números." onchange="checkForErrors(this.value)">
+                    <input type="number" name="cep" id="cep" class="form-control" placeholder="Digite somente números."
+                    onchange="validarCEP(this.value, document.querySelector('#cep'))">
                 </div>
                 <div class="col-3">
                     <i class="fas fa-city"></i>
                     <label class="form-label fw-bold" for="cidade">Cidade <abbr title="Este campo é obrigatório"> *</abbr></label>
-                    <input type="text" name="cidade" id="cidade" class="form-control" placeholder="Digite o nome da cidade." onchange="checkForErrors(this.value)">
+                    <input type="text" name="cidade" id="cidade" class="form-control" placeholder="Digite o nome da cidade." 
+                    onchange="validarCidade(this.value, document.querySelector('#cidade'))">
                 </div>
                 <div class="col-3">
                     <label for="uf" class="form-label fw-bold">UF <abbr title="Este campo é obrigatório."> *</abbr></label>
-                    <input type="text" name="uf" id="uf" class="form-control" placeholder="Digite somente as iniciais." onchange="checkForErrors(this.value)">
+                    <input type="text" name="uf" id="uf" class="form-control" placeholder="Digite somente as iniciais." 
+                    onchange="validarUF(this.value, document.querySelector('#uf'))">
                 </div>
                 <div class="col-3">
                     <i class="fas fa-flag"></i>
                     <label for="pais" class="form-label fw-bold">País <abbr title="Este campo é obrigatório."> *</abbr></label>
-                    <input type="text" name="pais" id="pais" class="form-control" placeholder="Digite o país." onchange="checkForErrors(this.value)">
+                    <input type="text" name="pais" id="pais" class="form-control" placeholder="Digite o país." 
+                    onchange="validarPais(this.value, document.querySelector('#pais'))">
                 </div>
             </div>
         </div>
@@ -129,9 +144,7 @@
 </form>
 
 <script>
-    const form = document.getElementsByTagName('form')[0];
-
-    form.addEventListener('submit', function(event) {
+    document.getElementsByTagName('form')[0].addEventListener('submit', function(event) {
 
         if (!input.validity.valid) {
             alert('Há dados inválidos no formulário.')
@@ -139,80 +152,112 @@
         }
     });
 
-    function checkForErrors(inputValue) {
+    function validarNomeCompleto(valor, elemento) {
+        
+        const regex = /^[\w\W]+$/i;
 
-        for (let i = 1; i < 17; i++) {
+        const validade = valor.split(/ +/).every(parte => regex.test(parte));
 
-            let input = document.getElementsByTagName('input')[i];
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido');
+    }
 
-            let name = input.getAttribute("name");
-            
-            let regex = [/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ']+$/i, /^\d{11}$/i,
-                        /^\d{9}$/i, /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i, /^\d{1,5}$/i,
-                        /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\d]+$/i, /^\d{8}$/i,
-                        /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ']{,3}$/i];
+    function validarCPF(valor, elemento) {
 
-            let validInput;
+        const regex = /^\d{11}$/i;
 
-            switch (name) {
-                case nome:
-                case mae:
-                case pai: 
-                case cidade:
-                case pais:
-                case logr:
+        const validade = regex.test(valor);
 
-                    validInput = inputValue.split(/ +/).every(parte => regex[0].test(parte));
-                    
-                    break;
-                case cpf: 
-                case tel1: 
-                case tel2:
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido');
+    }
 
-                    validInput = regex[1].test(inputValue);
+    function validarRG(valor, elemento) {
+        
+        const regex = /^\d{9}$/i;
 
-                    break;
-                case rg: 
+        const validade = regex.test(valor);
 
-                    validInput = regex[2].test(inputValue);;
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido'); 
+    }
 
-                    break;
-                case email: 
+    function validarEmail(valor, elemento) {
+               
+        const regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
 
-                    validInput = regex[3].test(inputValue);
+        const validade = regex.test(valor);
 
-                    break;
-                case num: 
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido');
+    }
 
-                    validInput = regex[4].test(inputValue);
-                    
-                    break;
-                case comp: 
+    function validarTelefone(valor, elemento) {
 
-                    validInput = inputValue.split(/ +/).every(parte => regex[5].test(parte));
+        const regex = /^\d{11}$/i;
 
-                    break;
+        const validade = regex.test(valor);
 
-                case cep: 
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido');
+    }
 
-                    validInput = regex[6].test(inputValue);
+    function validarLOGR(valor, elemento) {
 
-                    break;
-                case uf: 
+        const regex = /^[\w\W]+$/i;
+        
+        const validade = valor.split(/ +/).every(parte => regex.test(parte));
+        
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido');
+    }
 
-                    validInput = regex[7].test(inputValue);
+    function validarNumero(valor, elemento) {
 
-                    break;
-                default:
-                    break;
-            }
+        const regex = /^\d{1,}$/i;
 
-            if (validInput === false) {
-                input.setCustomValidity('Input inválido.');
-            } else {
-                input.setCustomValidity('');
-            }
-        }
+        const validade = regex.test(valor);
+
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido');
+    }
+
+    function validarCEP(valor, elemento) {
+        
+        const regex = /^\d{8}$/i;
+
+        const validade = regex.test(valor);
+
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido');
+    }
+
+    function validarComp(valor, elemento) {
+
+        const regex = /^[\w\W]+$/i;
+
+        const validade = valor.split(/ +/).every(parte => regex.test(parte));
+
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido');
+    }
+
+    function validarCidade(valor, elemento) {
+
+        const regex = /^[\w\W]+$/i;
+
+        const validade = valor.split(/ +/).every(parte => regex.test(parte));
+
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido'); 
+    }
+
+    function validarUF(valor, elemento) {
+
+        const regex = /^[A-Za-z]{2}$/i;
+
+        const validade = regex.test(valor);
+
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido');
+    }
+    
+    function validarPais(valor, elemento) {
+
+        const regex = /^[\w\W]+$/i;
+
+        const validade = valor.split(/ +/).every(parte => regex.test(parte));
+
+        validade ? elemento.setCustomValidity('') : elemento.setCustomValidity('Campo inválido')
     }
 </script>
 @endsection
